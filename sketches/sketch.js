a8eb5c8ddb14b1df307b7e3335bcb44bae2cf4c7
choiyejin0125 <174289831+choiyejin0125@users.noreply.github.com>
2025-06-20 09:25:06 +0900

    파일명 변경 주석 추가

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -14,17 +14,17 @@ const canvasH = 600;
 function setup() {
   let layout = new Responsive();
   layout.createResponsiveCanvas(canvasW, canvasH, 'contain', true);
-
+  // 웹캠 시작
   cam = createCapture(VIDEO);
   cam.size(camW, camH);
   cam.hide();
-
+  // 뿌연 레이어 만들기
   fogLayer = createGraphics(canvasW, canvasH);
   fogLayer.noStroke();
   fogLayer.fill(255, 200);
   fogLayer.rect(0, 0, canvasW, canvasH);
   fogLayer.filter(BLUR, 6);
-
+  // 손 트래킹 모델 준비
   handModel = ml5.handpose(cam, function () {});
   handModel.on('predict', function (results) {
     handResults = results;
@@ -33,19 +33,19 @@ function setup() {
 
 function draw() {
   background(0);
-
+  // 1. 웹캠 영상 좌우반전해서 그리기
   push();
   translate(canvasW, 0);
   scale(-1, 1);
   image(cam, 0, 0, canvasW, canvasH);
   pop();
-
+  // 2. 뿌연 레이어(수증기)와 물방울 그리기
   image(fogLayer, 0, 0);
-
+  // 3. 뿌연 레이어 천천히 복원 (더 느리게 흐려짐)
   if (frameCount % 5 === 0) {
     restoreFog(fogLayer, 200, 4); // maxOpacity = 200, fadeSpeed = 4
   }
-
+  // 4. 손가락 위치에 닦기 효과와 물방울 생성
   if (handResults.length > 0) {
     let landmarks = handResults[0].landmarks;
     fogLayer.loadPixels();
@@ -79,17 +79,18 @@ function draw() {
 
     fogLayer.updatePixels();
   }
-
+  // 5. 물방울 그리기 및 위치 업데이트
   updateWaterDrops(waterDrops, canvasH);
 }
 
+// 좌표 변환 함수
 function toCanvasCoords(x, y, camW, camH, canvasW, canvasH) {
   return {
     x: map(x, 0, camW, 0, canvasW),
     y: map(y, 0, camH, 0, canvasH),
   };
 }
-
+// 뿌연 레이어 알파값 서서히 복원
 function restoreFog(layer, maxOpacity, fadeSpeed) {
   layer.loadPixels();
 
@@ -137,7 +138,7 @@ function eraseCircle(layer, cx, cy, brushSize, fade, minOpacity) {
     }
   }
 }
-
+// 물방울 그리기
 function updateWaterDrops(waterDrops, canvasHeight) {
   for (let i = waterDrops.length - 1; i >= 0; i--) {
     let drop = waterDrops[i];
